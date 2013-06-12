d77346709bd804aaa9a2825ad2cf948af93e3fa9
Jonny Bergström <bergstrom@mjgarage.jp>
2013-06-12 17:16:21 +0900

    Breaks?

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Jonny Bergström on 2013/06/12.
+//  Copyright (c) 2013年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
