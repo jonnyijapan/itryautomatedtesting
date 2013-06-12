16152f3755f938f0b4592067f84575af7fe15883
Jonny Bergström <bergstrom@mjgarage.jp>
2013-06-12 16:48:02 +0900

    Commit midway

---
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.h
@@ -0,0 +1,13 @@
+//
+//  GuildBrowserLogicTests.h
+//  GuildBrowserLogicTests
+//
+//  Created by Jonny Bergström on 2013/06/12.
+//  Copyright (c) 2013年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GuildBrowserLogicTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/GuildBrowserLogicTests.m
@@ -0,0 +1,32 @@
+//
+//  GuildBrowserLogicTests.m
+//  GuildBrowserLogicTests
+//
+//  Created by Jonny Bergström on 2013/06/12.
+//  Copyright (c) 2013年 Charlie Fulton. All rights reserved.
+//
+
+#import "GuildBrowserLogicTests.h"
+
+@implementation GuildBrowserLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GuildBrowserLogicTests");
+}
+
+@end
